eb2467930e18d4380d377dc0d887348664f2d3f9
christophschmitz <c.schmitz@outlook.de>
2021-08-07 11:08:15 +0200

    ✨ Get coins in main from API

--- src/main.ts
+++ src/main.ts
@@ -4,38 +4,7 @@ import { createElement } from './utils/createElement';
 import { Coins } from './types';
 import { getCoins } from './utils/api';
 
-getCoins();
-
-const coins: Coins[] = [
-  {
-    name: 'Bitcoin',
-    icon: 'https://static.coinstats.app/coins/Bitcoin6l39t.png',
-    symbol: 'BTC',
-    rank: 1,
-    price: 34721.32,
-  },
-  {
-    name: 'ethereum',
-    icon: 'https://static.coinstats.app/coins/EthereumOCjgD.png',
-    symbol: 'ETH',
-    rank: 2,
-    price: 34721.32,
-  },
-  {
-    name: 'Bitcoin',
-    icon: 'https://static.coinstats.app/coins/Bitcoin6l39t.png',
-    symbol: 'BTC',
-    rank: 1,
-    price: 34721.32,
-  },
-  {
-    name: 'ethereum',
-    icon: 'https://static.coinstats.app/coins/EthereumOCjgD.png',
-    symbol: 'ETH',
-    rank: 2,
-    price: 34721.32,
-  },
-];
+const coins: Coins[] = await getCoins();
 
 const header = createElement('header', {
   childElements: [
